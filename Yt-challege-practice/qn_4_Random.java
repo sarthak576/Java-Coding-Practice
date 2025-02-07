d8cfa773e60df6041a808c2d26dce0541207c3be
sarthak576 <sarthakguptarjn@gmail.com>
2025-02-07 10:38:36 +0530

    qn-4 solved by merlin ai using while loop

--- Yt-challege-practice/qn_4_Random.java
+++ Yt-challege-practice/qn_4_Random.java
@@ -6,14 +6,18 @@ class qn_4_Random {
         // 0-200
         // avoid 2 digit numbers
         Random x = new Random();
-       int count = 1;
-        
-        while(count<21){
-        int random = x.nextInt(101)+100;
-        System.err.println(count+"="+random);
-count++;
+        int count = 1;
+
+        while (count < 21) {
+            int random = x.nextInt(101) + 100;
+            System.err.println(count + "=" + random);
+            count++;
         }
 
     }
-
+    // This program uses the java.util.Random class to generate random numbers.
+    // The nextInt(101) method generates a random integer between 0 and 100
+    // (inclusive).
+    // Adding 100 to the result shifts the range to 100-200 (inclusive).
+    // The while loop continues until 20 numbers have been generated and printed.
 }
